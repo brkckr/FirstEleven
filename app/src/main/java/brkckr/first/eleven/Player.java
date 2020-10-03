d146d4d177e326c145789a84133e387de92fdf9e
brkckr <burakcakir1991@gmail.com>
2020-10-04 02:40:29 +0300

    player added.

---
+++ app/src/main/java/brkckr/first/eleven/Player.java
@@ -0,0 +1,15 @@
+package brkckr.first.eleven;
+
+public class Player
+{
+    public String name;
+    public String profilePictureUrl;
+    public int number;
+
+    public Player(String name,String profilePictureUrl, int number)
+    {
+        this.name = name;
+        this.profilePictureUrl = profilePictureUrl;
+        this.number = number;
+    }
+}
